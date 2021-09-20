2182574fd5f64e60b23f6153c752058f3b2ef404
T.Chanatip <panda25735@gmail.com>
2021-09-20 14:35:28 +0700

    Section Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,20 @@ const tabs = document.querySelectorAll('[data-target]'),
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination"
+      ,clickable:true,
+    },
+    mousewheel: true,
+    keyboard: true
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
